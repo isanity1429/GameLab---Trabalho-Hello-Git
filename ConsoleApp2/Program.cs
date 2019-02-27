09174ee8389fe401fdf26048e69c5ba0d300b851
isanity1429 <hugosouza1429@hotmail.com>
2019-02-27 20:52:16 -0300

    Nova Funcionalidade Adicionada

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,7 +10,8 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Git:)");            
+            Console.WriteLine("Hello Git:)");
+            Console.WriteLine("[SEU NOME AQUI]");
         }
     }
 }
